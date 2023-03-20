d76ba9f10fa5911694a4de3d4798d2131fe389bf
Phol Castaneda <pholcast17@gmail.com>
2023-03-20 13:21:27 -0500

    Setting json file

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+let quiz = "Creando un quiz";
+let numero = 4;
+console.log(`${quiz} #${numero}`);
+console.log("phol cast");
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+
+let quiz:string ="Creando un quiz"
+
+let numero:number= 4
+console.log(`${quiz} #${numero}`);
+console.log("phol cast");
